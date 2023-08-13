9c94fb90a4cd336fe6c4529cca63dfb782c62289
yildirimsamet <yildirimsamet1@gmail.com>
2023-08-13 11:47:22 +0300

    EscapeSequences

---
+++ src/day02_EscapeSequences/EscapeSequences.java
@@ -0,0 +1,26 @@
+package day02_EscapeSequences;
+
+public class EscapeSequences {
+    public static void main(String[] args) {
+
+        System.out.println("Java \n Pyhton \n C#");
+
+        System.out.println("-----------------------");
+
+        System.out.println("Hello Everyone \n How are you all today \n It is nice to see you all! \n What class do we have next week?");
+
+        System.out.println("-----------------------");
+
+        System.out.println("\tJava is Cool Programming Language");
+
+        System.out.println("-------------------------");
+
+        // In order to  printone backward slash we have to give two backward slashes
+        System.out.println("/ \\");
+
+        System.out.println("----------------------------");
+
+        //In order to print double quot, we need to use \"
+        System.out.println("My favorite TV series is \"Breaking Bad\"");
+    }
+}
