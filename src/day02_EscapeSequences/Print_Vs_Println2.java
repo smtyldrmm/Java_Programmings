6c04091a85a7d7242c733551ae24dfebfe128fb3
yildirimsamet <yildirimsamet1@gmail.com>
2023-08-13 11:49:17 +0300

    Print_Vs_Println2

---
+++ src/day02_EscapeSequences/Print_Vs_Println2.java
@@ -0,0 +1,25 @@
+package day02_EscapeSequences;
+
+public class Print_Vs_Println2 {
+    public static void main(String[] args) {
+
+        System.out.println("Knock Knock");
+        System.out.println("Who is this");
+        System.out.println("This is Java");
+
+        System.out.println("---------------------------");
+
+        System.out.print("Knock Knock");
+        System.out.print("Who is this");
+        System.out.print("This is Java");
+
+        System.out.println("---------------------------");
+
+        System.out.println("Hello everyone, how are you all today? Today we will learn Escape Sequences, and next week we will learn Variables");
+        System.out.println("-------------------------");
+        System.out.print("Hello Everyone, How are you all today? ");
+        System.out.print("Today we will learn Escape Sequences ");
+        System.out.println(", and next week we will learn Variables");
+        System.out.println();
+    }
+}
