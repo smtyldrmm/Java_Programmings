61788a85e73f3f34dfb753341c01fc6349a5613a
yildirimsamet <yildirimsamet1@gmail.com>
2023-08-13 11:46:59 +0300

    Err_vs_Out

---
+++ src/day02_EscapeSequences/Err_vs_Out.java
@@ -0,0 +1,12 @@
+package day02_EscapeSequences;
+
+public class Err_vs_Out {
+    public static void main(String[] args) {
+
+        System.out.println("My name is Samet");
+
+        System.out.println("--------------------------");
+
+        System.err.println("My name is NOT Mahmut");
+    }
+}
