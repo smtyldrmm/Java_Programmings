d5d1ff13ffd3c4fc1c1978f2626d8a63af2d5723
yildirimsamet <yildirimsamet1@gmail.com>
2023-08-13 11:47:52 +0300

    GasReceipt with Escape Sequences

---
+++ src/day02_EscapeSequences/GasReceipt.java
@@ -0,0 +1,17 @@
+package day02_EscapeSequences;
+
+public class GasReceipt {
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("      MCLEAN STORE      ");
+        System.out.println();
+        System.out.println("13/08/2023 10:25pm");
+        System.out.println();
+        System.out.println("Gallons:       11.840");
+        System.out.println("Price/Gallon:  $2.089");
+        System.out.println();
+        System.out.println("Fuel Total   $ 24.734");
+
+    }
+}
