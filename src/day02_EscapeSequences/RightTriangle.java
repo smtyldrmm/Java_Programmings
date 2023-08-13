480140e81a1c64678ff80aca051f861981eb95c8
yildirimsamet <yildirimsamet1@gmail.com>
2023-08-13 11:49:34 +0300

    RightTriangle

---
+++ src/day02_EscapeSequences/RightTriangle.java
@@ -0,0 +1,25 @@
+package day02_EscapeSequences;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+        System.out.println("* * * * * *");
+        System.out.println("* * * * * * *");
+    }
+}
+/*
+Create a class called RightTriangle.java and print the following shape of right triangle
+
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+    * * * * * *
+    * * * * * * *
+ */
