76ea116138c6a59c8832a5c1e152e750e18a0230
yildirimsamet <yildirimsamet1@gmail.com>
2023-08-13 11:46:33 +0300

    EscapeSequences

---
+++ src/day02_EscapeSequences/AboutMe.java
@@ -0,0 +1,21 @@
+package day02_EscapeSequences;
+
+public class AboutMe {
+    public static void main(String[] args) {
+
+
+
+        System.out.println("\t\t\tMy favorite music is \"Rock\" music.\n\t\t\t\tMy favorite book is \"Data Structure in Java\"\n\t\t\t\t\t" +
+                "My favorite quote is \"It always seems impossible, until it is done\"\n\t\t\t\t\t\t My favorite TV series is \"Breaking Bad\" ");
+
+        System.out.println("\tMy favorite music is \"Rock\" music.\r\t\t\t\tMy favorite book is \"Data Structure in Java\"");
+    }
+}
+/*
+    Create a class called AboutMe.java
+    write a program to tell us about yourself
+        favorite music
+        favorite book
+        favorite quote
+        favorite TV series
+ */
