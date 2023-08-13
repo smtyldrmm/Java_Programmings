8da6aac8360aa6cb658e2127f86692066c5a6f02
yildirimsamet <yildirimsamet1@gmail.com>
2023-08-13 10:39:15 +0300

    Hello World

---
+++ src/day01_Java_Intro/HelloWorld.java
@@ -0,0 +1,9 @@
+package day01_Java_Intro;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println("Wooden Spoon");
+    }
+}
