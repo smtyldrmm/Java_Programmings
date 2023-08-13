d7789f53d0928ba4d3d02c1e7598e420a3a2fedd
yildirimsamet <yildirimsamet1@gmail.com>
2023-08-13 12:44:20 +0300

    PersonalInfo

---
+++ src/day02_EscapeSequences/PersonalInfo.java
@@ -0,0 +1,18 @@
+package day02_EscapeSequences;
+
+public class PersonalInfo {
+    public static void main(String[] args) {
+
+        System.out.println("Name: John");
+
+        System.out.println("Gender: Male");
+
+        System.out.println("Date of Birth: 06/06/1996");
+
+        System.out.println("Address: Los Angles, CA");
+
+        System.out.println( "Phone: (123-456-789)");
+
+        System.out.println("SSN: (***-***-***)");
+    }
+}
