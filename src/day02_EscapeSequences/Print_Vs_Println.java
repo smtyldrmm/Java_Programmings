9f0a7418cbe53887746be36822df428ba83a79aa
yildirimsamet <yildirimsamet1@gmail.com>
2023-08-13 11:49:01 +0300

    Print_Vs_Println

---
+++ src/day02_EscapeSequences/Print_Vs_Println.java
@@ -0,0 +1,21 @@
+package day02_EscapeSequences;
+
+public class Print_Vs_Println {
+    public static void main(String[] args) {
+
+        System.out.println("Hello everyone");
+        System.out.println("How are you all today?");
+        System.out.println("------------------------");
+
+        System.out.print("Hello everyone ");
+        System.out.print("How are you all today?");
+
+        System.out.println("Java Programming");
+
+        System.out.print("Wooden Spoon ");
+
+        System.out.println("I love you  Java");
+
+        System.out.println("I love you Data Structure");
+    }
+}
