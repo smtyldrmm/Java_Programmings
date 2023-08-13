7f3aa82bf6a733649c6d5068f2fed416dfdf584c
yildirimsamet <yildirimsamet1@gmail.com>
2023-08-13 10:50:19 +0300

    Print Execution

---
+++ src/day01_Java_Intro/PrintExecution.java
@@ -0,0 +1,15 @@
+package day01_Java_Intro;
+
+public class PrintExecution {
+    public static void main(String[] args) {
+
+        System.out.println("Samet");
+        System.out.println("Kayra");
+        System.out.println();
+        System.out.println("Sevinc");
+        System.out.println("Yildirim");
+        System.out.println();
+        System.out.println("Uras");
+        System.out.println("Afra");
+    }
+}
