3d524caaf5cf2426a969ed2a07ec9a9aaa96c2c1
yildirimsamet <yildirimsamet1@gmail.com>
2023-08-17 14:21:28 +0300

    EmployeeInfo

---
+++ src/day03_Variables/EmployeeInfo.java
@@ -0,0 +1,32 @@
+package day03_Variables;
+
+public class EmployeeInfo {
+    public static void main(String[] args) {
+
+            String employeeName="Samet";
+            int age=33;
+            char gender='M';
+            String companyName="Trendyol";
+            String jobTitle="Backend Developer";
+            double yearsOfExperience=1.5;
+            int salary=110000;
+            boolean isFullTime=true;
+            boolean isMarried=true;
+            String employeeId="A54B3";
+            String SSN="(123-456-78)";
+
+        System.out.println("employeeName = " + employeeName);
+        System.out.println("age = " + age);
+        System.out.println("gender = " + gender);
+        System.out.println("companyName = " + companyName);
+        System.out.println("jobTitle = " + jobTitle);
+        System.out.println("yearsOfExperience = " + yearsOfExperience);
+        System.out.println("salary = " + salary);
+        System.out.println("isFullTime = " + isFullTime);
+        System.out.println("isMarried = " + isMarried);
+        System.out.println("employeeId = " + employeeId);
+        System.out.println("SSN = " + SSN);
+
+
+    }
+}
