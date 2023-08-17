0b1ddf6c262eaca10a143a0b72c1f73536eea868
yildirimsamet <yildirimsamet1@gmail.com>
2023-08-17 14:25:05 +0300

    GallonsToLiters

---
+++ src/day03_Variables/GallonsToLiters.java
@@ -0,0 +1,14 @@
+package day03_Variables;
+
+public class GallonsToLiters {
+    public static void main(String[] args) {
+
+        int gallons=1000;
+        double liters=gallons*3.79;
+        System.out.println("liters = " + liters);
+    }
+}
+/*
+5. Create a class named GallonsToLitters, Write a program that can convert gallon (int) to litter (double)
+        Hints: 1 gallon = 3.79 litters
+        */
