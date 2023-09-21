e5da0a8983cc4ef32348a3c42c3fa70f635dc08d
yildirimsamet <yildirimsamet1@gmail.com>
2023-09-21 17:21:12 +0300

    ShippingAddress

---
+++ src/day05_Concatenation/StudentInfo.java
@@ -0,0 +1,24 @@
+package day05_Concatenation;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        String student_name = "Tamara";
+        int age = 25;
+        char gender = 'F';
+        String schoolName = "Cydeo";
+        String phoneNumber1 = "(123)-456-7891";
+        String phoneNumber2 = "(124)-454-7891";
+        boolean isFullTime = true;
+        double GPA = 3.5;
+
+        System.out.println("student_name = " + student_name);
+        System.out.println("age = " + age);
+        System.out.println("gender = " + gender);
+        System.out.println("schoolName = " + schoolName);
+        System.out.println("phoneNumber = " + phoneNumber1);
+        System.out.println("phoneNumber2 = " + phoneNumber2);
+        System.out.println("GPA = " + GPA);
+        System.out.println("isFullTime = " + isFullTime);
+
+    }
+}
