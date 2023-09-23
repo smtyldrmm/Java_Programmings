1891686efb0747b4eb712de606810a7e71cac1c3
yildirimsamet <yildirimsamet1@gmail.com>
2023-09-23 10:33:05 +0300

    Birthday

---
+++ src/day06_PrimitiveTypeCasting/BirthDay.java
@@ -0,0 +1,47 @@
+package day06_PrimitiveTypeCasting;
+
+public class BirthDay {
+    public static void main(String[] args) {
+        String name = "Aaron";
+        int birthDay = 25;
+        String month = "April";
+        int birthYear = 1995;
+
+        //John was born on month/day/year.
+        System.out.println(name + " was born on " + month + "/" +birthDay +"/"+birthYear );
+
+        System.out.println("-----------------------------------------------");
+
+        // My favorite book is "bookName"
+        String bookName = "The Rich Dad and The Poor Dad";
+
+        System.out.println("My favorite book is " +  "\"" +bookName + "\"");
+        /*
+        \n new line
+        \t tab
+        \" double quote
+        \\ backward slash
+
+         */
+
+
+
+
+    }
+
+}
+
+/*
+1. Create a class named BirthDay and create the following variable
+			 name, birthDay(int), birthMonth(String), birthYear(int)
+
+			 use concatenation to display the birthday of the person
+
+			 	if  name = "John"
+			 		birthDay = 25
+			 	    birthMonth = "April"
+			 	    birthYear = 1995;
+
+			 	output:
+			 		John was born on April/25/1995.
+ */
