acb1b0e06ec61e7faa594d945e27c3f2f5856a7a
yildirimsamet <yildirimsamet1@gmail.com>
2023-10-16 23:39:30 +0300

    FrequencyOfWord2

---
+++ src/Day17_While_DoWhile/FrequencyOfWord2.java
@@ -0,0 +1,24 @@
+package Day17_While_DoWhile;
+
+public class FrequencyOfWord2 {
+    public static void main(String[] args) {
+
+
+        String  str = "Cat Cat Dog Dog cAt CAT caT";
+
+        int frequency = 0; // it is temporary variable
+
+        for (int i = 0; i <= str.length()-3; i++) {
+            String eachSub =    str.substring(i, i+3 );
+
+            if(eachSub.equalsIgnoreCase("Cat")){
+                // equalsIgnoreCase doesn't care how it was written
+                frequency++;
+            }
+
+        }
+
+        System.out.println(frequency);
+
+    }
+}
