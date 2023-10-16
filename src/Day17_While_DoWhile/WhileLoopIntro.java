41930318c2ebf2e8b11df55812f2b5512b6e0abf
yildirimsamet <yildirimsamet1@gmail.com>
2023-10-16 18:04:33 +0300

    WhileLoopIntro

---
+++ src/Day17_While_DoWhile/WhileLoopIntro.java
@@ -0,0 +1,21 @@
+package Day17_While_DoWhile;
+
+public class WhileLoopIntro {
+    public static void main(String[] args) {
+
+
+        if(true){
+            System.out.println("Wooden Spoon"); // 1 time
+        }
+
+
+        System.out.println("-----------------------------------------");
+
+        while(true){ // repeated if
+            System.out.println("Wooden Spoon"); //
+        } // it will print infinite times
+
+
+    }
+
+}
