6373007d4005b0daa950598be922b65dba2f3955
yildirimsamet <yildirimsamet1@gmail.com>
2023-10-17 17:23:51 +0300

    DoWhileLoopIntro

---
+++ src/Day17_While_DoWhile/DoWhileLoopIntro.java
@@ -0,0 +1,33 @@
+package Day17_While_DoWhile;
+
+public class DoWhileLoopIntro {
+    public static void main(String[] args) {
+
+
+        boolean s = false;
+
+        for(int i = 0; s;){
+            System.out.println("Wooden Spoon -- for loop");
+        }
+
+        System.out.println("***********************");
+
+        while(s){
+            System.out.println("Wooden Spoon -- while loop");
+        }
+
+        System.out.println("************************");
+
+
+        do{
+            System.out.println("Wooden Spoon -- do-while loop");
+        }while(s);
+
+
+
+
+    }
+
+
+}
+
