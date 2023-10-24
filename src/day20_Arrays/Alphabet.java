58a289cededbdac6133504abddd523eb38d0ae50
yildirimsamet <yildirimsamet1@gmail.com>
2023-10-24 17:49:51 +0300

    Alphabet

---
+++ src/day20_Arrays/Alphabet.java
@@ -0,0 +1,30 @@
+package day20_Arrays;
+
+import java.util.Arrays;
+
+public class Alphabet {
+    public static void main(String[] args) {
+
+        // toString = convert array to String
+        char [] alphabet=new char[26]; // Z-A
+        alphabet[0]='Z';
+        // System.out.println(Arrays.toString(alphabet)); //print the whole array
+        // System.out.println(alphabet[0]);              // print the element of array so it gives us a char
+        char ch='Z';
+        for (int i = 0; i <alphabet.length ; i++) {
+            alphabet[i]=ch--;
+
+        }
+        System.out.println(Arrays.toString(alphabet));
+
+        for(char a=0, b='Z';a<alphabet.length; a++, b--){
+            alphabet[a]=b;
+        }
+        System.out.println(Arrays.toString(alphabet));
+
+
+    }
+
+
+}
+
