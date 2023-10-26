5aeb66786764bcb5e1acabc5fd63007133b8d001
yildirimsamet <yildirimsamet1@gmail.com>
2023-10-26 15:18:54 +0300

    NestedLoop

---
+++ src/day18_NestedLoop/NestedLoop.java
@@ -0,0 +1,20 @@
+package day18_NestedLoop;
+
+public class NestedLoop { public static void main(String[] args) {
+
+    for (int i = 0; i < 5; i++) {
+        System.out.println("Wooden Spoon");
+
+    }
+    System.out.println("-------------------");
+
+    for (int j = 0; j <4 ; j++) {
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("Samet Kayra");
+
+        }
+
+    }
+}
+
+}
