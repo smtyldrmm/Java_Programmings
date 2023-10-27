46ac6d63c0b9fa77350e6b5c51517a9cafee9ac3
yildirimsamet <yildirimsamet1@gmail.com>
2023-10-27 13:58:35 +0300

    LoopsWithoutCurlyBraces

---
+++ src/day19_LoopPractice/LoopsWithoutCurlyBraces.java
@@ -0,0 +1,16 @@
+package day19_LoopPractice;
+
+public class LoopsWithoutCurlyBraces {
+    public static void main(String[] args) {
+
+        // If there will be one row, I can use statement without curly braces
+
+        for (int i = 0; i < 5; i++)
+            System.out.println("Cydeo");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Batch 25");
+        }
+
+    }
+}
