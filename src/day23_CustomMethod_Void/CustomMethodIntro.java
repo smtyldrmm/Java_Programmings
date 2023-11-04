e18de3dea2783b1ceb58f09513cf3af6d2573504
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-04 15:58:33 +0300

    CustomMethodIntro

---
+++ src/day23_CustomMethod_Void/CustomMethodIntro.java
@@ -0,0 +1,21 @@
+package day23_CustomMethod_Void;
+
+public class CustomMethodIntro {
+    public static void main(String[] args) {
+
+        greeting();
+        greeting();
+    }
+
+
+
+
+    public static void greeting (){
+
+
+        System.out.println("Hello Cydeo");
+        System.out.println("How are you today?");
+        System.out.println("Hi I'm Samet");
+    }
+    }
+
