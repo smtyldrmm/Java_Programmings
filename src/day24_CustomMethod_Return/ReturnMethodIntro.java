25e0172b17122e91f68f803a9183f33cd2e10e27
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-06 11:46:09 +0300

    ReturnMethodIntro

---
+++ src/day24_CustomMethod_Return/ReturnMethodIntro.java
@@ -0,0 +1,20 @@
+package day24_CustomMethod_Return;
+
+public class ReturnMethodIntro {
+    public static void main(String[] args) {
+        String str="Samet";
+        String result=reverse(str);
+        System.out.println(result);
+    }
+    public static String reverse(String str){
+        String reverse="";
+        for (int i = str.length()-1; i >=0 ; i--) {
+            reverse+=str.charAt(i);
+
+
+        }
+        return reverse;
+    }
+
+}
+
