831eca0d730715aa1ca836cbc0b8817f97ddd680
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-07 14:58:12 +0300

    ReturnMethodPractice2

---
+++ src/day24_CustomMethod_Return/ReturnMethodPractice2.java
@@ -0,0 +1,32 @@
+package day24_CustomMethod_Return;
+
+public class ReturnMethodPractice2 {
+    public static void main(String[] args) {
+        String str=grade(95);
+        if(str.equals("A")){
+            System.out.println("Excellent");
+        } else if (str.equals("B")) {
+            System.out.println("Great");
+        } else if (str.equals("C")) {
+            System.out.println("Good");
+        } else if (str.equals("D")) {
+            System.out.println("Passed");
+        }else {
+            System.out.println("Try Again");
+        }
+    }
+    public static String grade(int score){
+        String result="";
+        if(score<0 && score>100){
+            result="Invalid Score";
+        }else {
+            result=(score>=90)? "A" : (score>=80)? "B" : (score >=70)? "C" : (score>=60)? "D" : "F" ;
+        }
+        return result;
+    }
+}
+
+
+
+
+
