6a75742f06989addc6d4abcb12f05e2c78b5da8b
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-09 12:13:13 +0300

    ReverseArray.java

---
+++ src/day26_CustomMethodPractice/ReverseArray.java
@@ -0,0 +1,60 @@
+package day26_CustomMethodPractice;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5};
+        int [] result=reverse(arr);
+        System.out.println(Arrays.toString(result));
+    }
+    public static int [] reverse(int [] array){
+        int [] result=new int[array.length];
+        for (int i = array.length - 1, j=0; i >= 0; i--,j++) {
+            result[j]=array[i];
+
+
+
+        }
+        return result;
+
+
+    }
+    public static double [] reverse(double [] array){
+        double [] result=new double[array.length];
+        for (int i = array.length - 1, j=0; i >= 0; i--,j++) {
+            result[j]=array[i];
+
+
+
+        }
+        return result;
+
+
+    }
+    public static char [] reverse(char [] array){
+        char [] result=new char[array.length];
+        for (int i = array.length - 1, j=0; i >= 0; i--,j++) {
+            result[j]=array[i];
+
+
+
+        }
+        return result;
+
+
+    }
+    public static String [] reverse(String [] array){
+        String [] result=new String[array.length];
+        for (int i = array.length - 1, j=0; i >= 0; i--,j++) {
+            result[j]=array[i];
+
+
+
+        }
+        return result;
+
+
+    }
+}
