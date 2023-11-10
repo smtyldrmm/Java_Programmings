777778e92e6ef97a1604b8c09469ff33e6f62926
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-10 21:10:55 +0300

    NthLargestNumber.java

---
+++ src/day29_ArrayList/NthLargestNumber.java
@@ -0,0 +1,28 @@
+package day29_ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class NthLargestNumber {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list=new ArrayList<>();
+
+
+        list.addAll(Arrays.asList(1,2,3,4,5,6,7,7,8,8));
+
+        int n=1;
+
+        for (int i = 1; i < n ; i++) {
+            list.removeIf(p -> Collections.max(list)==p);// Döngü kaç kere dönerse o sayıya denk gelen en büyük sayıyı verecek.
+        }
+        int max= Collections.max(list);
+
+        System.out.println(max);
+
+
+    }
+}
+
+/*Write a program that can return the nth largest number from arraylist*/
