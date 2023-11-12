49a0e101e7405f30bb6d0a89a1b3bfb555c8e28a
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-12 23:01:06 +0300

    Person.java

---
+++ src/day31_Constructor/PersonObject.java
@@ -0,0 +1,15 @@
+package day31_Constructor;
+
+public class PersonObject {
+    public static void main(String[] args) {
+    Person person1=new Person("Samet",'M',33);
+    System.out.println(person1);
+
+    Person person2=new Person("Kayra",'M',2);
+    System.out.println(person2);
+
+
+}
+}
+
+
