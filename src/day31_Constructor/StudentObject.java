9fa4d1bfbf029da37fad1aec699e40aa0222313b
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-13 17:15:38 +0300

    StudentObject.java

---
+++ src/day31_Constructor/StudentObject.java
@@ -0,0 +1,17 @@
+package day31_Constructor;
+
+public class StudentObject {
+    public static void main(String[] args) {
+
+    Student student1=new Student("Samet",33,'M','A',510);
+
+    Student student2=new Student("Kayra",2,'M','B',459);
+
+    Student student3=new Student("Sevinc",32,'F','C',789);
+
+    System.out.println(student1);
+    System.out.println(student2);
+}
+}
+
+
