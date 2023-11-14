b2563a88c720e23d9b4cebf34cb177b63a3dd69f
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-14 12:01:01 +0300

    CarpetObject.java

---
+++ src/day32_Constrcutor/CarpetObject.java
@@ -0,0 +1,11 @@
+package day32_Constrcutor;
+
+public class CarpetObject {
+    public static void main(String[] args) {
+    Carpet carpet1=new Carpet(5.2,10.0,100,true);
+    System.out.println(carpet1);
+
+    Carpet carpet2=new Carpet(3.0,2.5,50,false);
+    System.out.println(carpet2);
+}
+}
