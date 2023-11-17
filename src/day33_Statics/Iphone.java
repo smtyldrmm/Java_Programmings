78944f677c9e582ad9ffcc6312a103550e9d31f7
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-17 23:59:44 +0300

    Iphone.java

---
+++ src/day33_Statics/Iphone.java
@@ -0,0 +1,31 @@
+package day33_Statics;
+
+public class Iphone {
+    public static String brand="Apple";
+
+    public String model;
+
+    public double price;
+
+    public static String operatingSystem="IOS";
+
+    public String color;
+
+    public String size;
+
+    public static String madeIn="China";
+
+    public static boolean hasBattery=true;
+
+    public static boolean hasTouchScreen=true;
+
+    public static void prinOperatingSystem()
+    {
+
+        System.out.println(operatingSystem);
+    }
+
+
+
+}
+
