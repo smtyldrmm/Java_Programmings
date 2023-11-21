26e00c814873a8cb18e3e1621d7d1d0d2fd01707
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-21 22:25:09 +0300

    StaticImport.java

---
+++ src/day34_GarbageCollections_AccessModifier/StaticImport.java
@@ -0,0 +1,14 @@
+package day34_GarbageCollections_AccessModifier;
+
+
+
+import static day34_GarbageCollections_AccessModifier.Circle.pi;
+
+public class StaticImport {
+    public static void main(String[] args) {
+
+
+        System.out.println(pi);
+
+    }
+}
