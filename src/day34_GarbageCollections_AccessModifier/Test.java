95a48f49b7f4215a838a909773083e592a938681
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-21 22:25:38 +0300

    Test.java

---
+++ src/day34_GarbageCollections_AccessModifier/Test.java
@@ -0,0 +1,15 @@
+package day34_GarbageCollections_AccessModifier;
+
+import static day34_GarbageCollections_AccessModifier.Circle.*;
+
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(pi);
+        System.out.println(name);
+
+
+
+
+    }
+}
