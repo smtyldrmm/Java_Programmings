2edd9ba763a9ad3498f6aa81b83a01fa51da3f53
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-24 20:28:13 +0300

    StudentObject.java

---
+++ src/day36_Inheritance/Encapsulation/StudentObject.java
@@ -0,0 +1,11 @@
+package day36_Inheritance.Encapsulation;
+
+public class StudentObject {
+        public static void main(String[] args) {
+    Student student1=new Student("Samet",2,'M','A',"Cydeo");
+    student1.setAge(33);
+
+    System.out.println(student1);
+
+}
+}
