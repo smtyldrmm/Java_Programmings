221413478af659f832590d42a9ac685c25defddd
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-24 15:35:31 +0300

    Employee.java

---
+++ src/day35_Encapsulation/encapsulation/Employee.java
@@ -0,0 +1,60 @@
+package day35_Encapsulation.encapsulation;
+
+public class Employee {
+
+    private String name;
+    private char gender;
+    private int age;
+    private double salary;
+
+    public Employee(String name, char gender, int age, double salary) {
+        setName(name);
+        setGender(gender);
+        setAge(age);
+        setSalary(salary);
+        // Yukarıdaki şekilde yaparsak aşağıdaki method içerisinde setter bölümünde bir şarta bağlıysa
+        // bu şartı uygulatacak şekilde constructor kısmını çalıştırabiliriz.
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", gender=" + gender +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
+}
