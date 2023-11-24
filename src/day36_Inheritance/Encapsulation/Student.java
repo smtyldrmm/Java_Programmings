4a18f81cf53ca696a3f0c86818048fbe61fdbd36
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-24 20:27:47 +0300

    Student.java

---
+++ src/day36_Inheritance/Encapsulation/Student.java
@@ -0,0 +1,72 @@
+package day36_Inheritance.Encapsulation;
+
+public class Student { private String name;
+    private int age;
+    private char gender, grade;
+    private String schoolName;
+
+    public Student(String name, int age, char gender, char grade, String schoolName) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+        setGrade(grade);
+        setSchoolName(schoolName);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age<=5 || age >=90)
+            return; // exits the method / burada return kullanmamızın void ile alakası var
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        if(!(gender=='M' || gender=='F'))
+            return; // exits the method
+        this.gender = gender;
+    }
+
+    public char getGrade() {
+        return grade;
+    }
+
+    public void setGrade(char grade) {
+        if(!(grade=='A' || grade=='B' || grade=='C' || grade=='D' || grade=='F'))
+            return;
+        this.grade = grade;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", grade=" + grade +
+                ", schoolName='" + schoolName + '\'' +
+                '}';
+    }
+}
