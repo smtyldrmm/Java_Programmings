b414555137f5f99b1d5b0764ba1803d48be3ab81
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-24 20:25:41 +0300

    Dog.java

---
+++ src/day36_Inheritance/AnimalTask/Dog.java
@@ -0,0 +1,11 @@
+package day36_Inheritance.AnimalTask;
+
+public class Dog extends Animal{ // Dog is an animal
+
+    public void bark(){
+        System.out.println(name+" is barking");
+    }
+
+
+
+}
