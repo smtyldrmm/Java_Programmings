6e8fab703662e4362cb40a57c60567de497da597
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-25 10:20:38 +0300

    Cat.java

---
+++ src/day37_Inheritance/AnimalTask/Cat.java
@@ -0,0 +1,13 @@
+package day37_Inheritance.AnimalTask;
+
+public class Cat extends Animal {
+
+
+    public Cat(String name, String breed, char gender, String size, int age, String color) {
+        super(name, breed, gender, size, age, color);
+    }
+
+    public void scratch(){
+        System.out.println(name+ " is scratching");
+    }
+}
