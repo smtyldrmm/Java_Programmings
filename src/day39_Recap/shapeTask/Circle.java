e3277aedf1b73203d60e0545b6530aa4525faabd
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-26 20:28:53 +0300

    Circle.java

---
+++ src/day39_Recap/shapeTask/Circle.java
@@ -0,0 +1,46 @@
+package day39_Recap.shapeTask;
+
+public class Circle extends Shape{
+
+    private double radius;
+    public static double pi = 3.14;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if(radius <= 0){
+            System.err.println("Invalid Radius: "+radius);
+            System.exit(1);
+        }
+        this.radius = radius;
+    }
+
+    public Circle( double radius) {
+        super("Circle");
+        setRadius(radius);
+    }
+
+
+    public double area() {
+        return radius* radius* pi;
+    }
+
+
+    public double perimeter() {
+        return 2*radius*pi;
+    }
+
+
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", pi='" + pi + '\'' +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
+
+
+}
