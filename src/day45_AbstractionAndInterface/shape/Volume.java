83c504851d589d11a25d445e156d39de38615f38
yildirimsamet <yildirimsamet1@gmail.com>
2023-11-29 13:58:08 +0300

    Volume.java

---
+++ src/day45_AbstractionAndInterface/shape/Volume.java
@@ -0,0 +1,15 @@
+package day45_AbstractionAndInterface.shape;
+
+public interface Volume {
+
+    boolean hasVolume= true;
+
+    double volume();
+
+   /*
+    default void drink(){
+         System.out.println("Drinks water");
+     }
+   */
+
+}
