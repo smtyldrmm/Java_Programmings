5560c2205bf64607be8d46ed07fde07e5a0469c7
yildirimsamet <yildirimsamet1@gmail.com>
2023-12-07 12:06:29 +0300

    AccesModifierTest.java

---
+++ src/utilities/AccesModifierTest.java
@@ -0,0 +1,28 @@
+package utilities;
+
+import day34_GarbageCollections_AccessModifier.AccessModifier;
+
+public class AccesModifierTest {
+
+    public static void main(String[] args) {
+
+    System.out.println( AccessModifier.publicData );
+    //   System.out.println( AccessModifiers.defaultData ); //default Access Modifier is not visible outside the package
+    //   System.out.println( AccessModifiers.privateData ); //private access modifier is not visible outside class
+
+
+        AccessModifier.method1();
+    //     AccessModifiers.method2();
+        AccessModifier.method3();
+
+
+
+}
+
+}
+
+
+
+class D{
+
+}
