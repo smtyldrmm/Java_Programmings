c3a9a586b13ecb8b9cfeffc7d66e46d1ad03ed42
yildirimsamet <yildirimsamet1@gmail.com>
2023-12-09 21:42:25 +0300

    Balance.java

---
+++ src/day54_java_ecommerce_project/src/balance/Balance.java
@@ -0,0 +1,22 @@
+package day54_java_ecommerce_project.src.balance;
+
+import java.util.UUID;
+
+public abstract class Balance {
+    private UUID customerId;
+    private Double balance;
+    public Balance(UUID customerId, Double balance) {
+        this.customerId = customerId;
+        this.balance = balance;
+    }
+    public abstract Double addBalance(Double additionalBalance);
+    public UUID getCustomerId() {
+        return customerId;
+    }
+    public Double getBalance() {
+        return balance;
+    }
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+}
