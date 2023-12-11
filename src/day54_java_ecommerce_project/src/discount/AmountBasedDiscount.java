3bd2d06149f1cce5e8ab19ca36b27aa0630a62b5
yildirimsamet <yildirimsamet1@gmail.com>
2023-12-11 23:46:20 +0300

    AmountBasedDiscount.java

---
+++ src/day54_java_ecommerce_project/src/discount/AmountBasedDiscount.java
@@ -0,0 +1,21 @@
+package day54_java_ecommerce_project.src.discount;
+import java.util.UUID;
+public class AmountBasedDiscount extends Discount{
+
+    private Double discountAmount;
+
+
+    public AmountBasedDiscount(UUID id, String name, Double thresholdAmount, Double discountAmount) {
+        super(id, name, thresholdAmount);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    public Double calculateCartAmountAfterDiscountApplied(Double amount) {
+        return amount - discountAmount;
+    }
+
+    public Double getDiscountAmount() {
+        return discountAmount;
+    }
+}
