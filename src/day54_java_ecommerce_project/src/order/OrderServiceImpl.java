f6cf2a01acf159fcb7a8997518f1e52a1cc35b0c
yildirimsamet <yildirimsamet1@gmail.com>
2023-12-11 23:47:57 +0300

    OrderServiceImpl.java

---
+++ src/day54_java_ecommerce_project/src/order/OrderServiceImpl.java
@@ -0,0 +1,68 @@
+package day54_java_ecommerce_project.src.order;
+
+
+import day54_java_ecommerce_project.src.Cart;
+import day54_java_ecommerce_project.src.checkout.CheckoutService;
+import day54_java_ecommerce_project.src.checkout.CustomerBalanceCheckoutServiceImpl;
+import day54_java_ecommerce_project.src.checkout.MixPaymentCheckoutServiceImpl;
+import day54_java_ecommerce_project.src.discount.Discount;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+import java.util.UUID;
+
+import static day54_java_ecommerce_project.src.StaticConstants.DISCOUNT_LIST;
+import static day54_java_ecommerce_project.src.StaticConstants.ORDER_LIST;
+
+public class OrderServiceImpl implements OrderService{
+    @Override
+    public String placeOrder(Cart cart) {
+        double amountAfterDiscount = cart.calculateCartTotalAmount();
+
+
+        if (cart.getDiscountId() != null){
+            try {
+                Discount discount = findDiscountById(cart.getDiscountId());
+                amountAfterDiscount = discount.calculateCartAmountAfterDiscountApplied(amountAfterDiscount);
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("which payment option you would like to choose, Type 1 : customer balance, Type 2 : Mix (gift card + customer balance)");
+        int paymentType = scanner.nextInt();
+        boolean checkoutResult = false;
+        switch (paymentType){
+            case 1:
+                CheckoutService customerBalanceCheckoutService = new CustomerBalanceCheckoutServiceImpl();
+                checkoutResult = customerBalanceCheckoutService.checkout(cart.getCustomer(), amountAfterDiscount);
+                break;
+            case 2:
+                CheckoutService mixPaymentCheckoutService = new MixPaymentCheckoutServiceImpl();
+                checkoutResult = mixPaymentCheckoutService.checkout(cart.getCustomer(), amountAfterDiscount);
+                break;
+        }
+
+
+        if (checkoutResult){
+            Order order = new Order(UUID.randomUUID(), LocalDateTime.now(),
+                    cart.calculateCartTotalAmount(), amountAfterDiscount,
+                    cart.calculateCartTotalAmount() - amountAfterDiscount, cart.getCustomer().getId()
+                    , "Placed", cart.getProductMap().keySet());
+            ORDER_LIST.add(order);
+            return "Order has been placed successfully";
+        }else {
+            return "Balance is insufficient. Please add money to your one of balances and try again.";
+        }
+    }
+
+    private Discount findDiscountById(UUID discountId) throws Exception {
+        for (Discount discount : DISCOUNT_LIST){
+            if (discount.getId().toString().equals(discountId.toString())){
+                return discount;
+            }
+        }
+        throw new Exception("Discount couldn't found");
+    }
+}
